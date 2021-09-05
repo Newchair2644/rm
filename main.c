0b8d07c819c6fdf76a0a74c27c1c1c40391f7e24
Numair Dubas <69362192+NumenHackerCracker@users.noreply.github.com>
2021-09-05 22:01:29 +0000

    clean-up

--- main.c
+++ main.c
@@ -5,77 +5,6 @@
 #include <dirent.h>
 #include <errno.h>
 
-int mode[4] = { 0, 0, 0, 0 };
-enum { NORMAL_MODE, FORCE_MODE, INTERACTIVE_MODE, RECURSIVE_MODE };
-
-void confirm(char *file);
-int isfile(const char *name);
-
-int main(int argc, char *argv[])
-{
-	// No arguments given
-	if (argc <= 1) {
-		fprintf(stderr, "rm: missing operand\n" \
-				"Try rm -h for more information\n");
-		return EXIT_FAILURE;
-	}
-
-	size_t optind;
-	for (optind = 1; optind < argc && argv[optind][0] == '-'; optind++) {
-		for (size_t optinl = 1;  optinl < strlen(argv[optind]); optinl++) {
-			switch (argv[optind][optinl]) {
-				// Force mode: remove files even if write-protected
-				case 'f': mode[FORCE_MODE] = 1; break;
-				// Interactive mode: prompt for confirmation
-				case 'i': mode[INTERACTIVE_MODE] = 1; break;
-				// Recursive mode: remove files and directories
-				case 'R': case 'r': mode[RECURSIVE_MODE] = 1; break;
-				default:
-					fprintf(stderr, "Usage: rm [-fiRr]... [FILE...]\n");
-					return EXIT_FAILURE;
-			}
-		}
-	}
-	if (optind == 1)
-		mode[NORMAL_MODE] = 1;
-
-	argv += optind; // increment by the number of options, leaving us with the files
-	int incorrect_file = 0; // wheather or not user has made an incorrect input
-	for (char **file = argv; *file; file++) {
-		if (access(*file, F_OK) != 0 && !mode[FORCE_MODE]) {
-			fprintf(stderr, "rm: cannot remove '%s' No such file or directory\n", *file);
-			incorrect_file = 1;
-			break;
-		}
-
-		if (mode[FORCE_MODE] && (mode[RECURSIVE_MODE] || isfile(*file))) {
-			remove(*file);
-		} else if (mode[INTERACTIVE_MODE] || access(*file, W_OK) != 0) {
-			confirm(*file);
-			// switch to normal mode so we still check if it is a file (unless recursive mode is on)
-			if (!mode[RECURSIVE_MODE])
-				mode[NORMAL_MODE] = 1;
-		}
-		// Normal mode (default): remove files if they exist or if they are not dirs
-		if ((mode[NORMAL_MODE] && isfile(*file)) || mode[RECURSIVE_MODE])
-			remove(*file);
-		else
-			incorrect_file = 1;
-
-	}
-	// if any of the files were invalid, return failure signal
-	return incorrect_file ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
-// confim deletion
-void confirm(char *file)
-{
-	fprintf(stderr, "rm: remove '%s'? ", file);
-	char confirm = getchar();
-	if (confirm != 'y')
-		exit(EXIT_FAILURE);
-}
-
 // check if dir
 int isfile(const char *name)
 {
@@ -91,4 +20,62 @@ int isfile(const char *name)
 		return 1;
 	// does not exist
 	return -1;
+}
+
+int main(int argc, char *argv[])
+{
+	int mode[3] = { 0, 0, 0 };
+	enum { force, interactive, recursive };
+
+	// No arguments given
+	if (argc <= 1) {
+		fprintf(stderr, "rm: missing operand\n" \
+				"Try rm -h for more information\n");
+		return 1;
+	}
+
+	size_t optind, optinl;
+	for (optind = 1; optind < argc && argv[optind][0] == '-'; optind++) {
+		for (optinl = 1;  optinl < strlen(argv[optind]); optinl++) {
+			switch (argv[optind][optinl]) {
+				// Force mode: remove files even if write-protected
+				case 'f': mode[force] = 1; break;
+				// Interactive mode: prompt for confirmation
+				case 'i': mode[interactive] = 1; break;
+				// Recursive mode: remove files and directories
+				case 'R': case 'r': mode[recursive] = 1; break;
+				default:
+					fprintf(stderr, "Usage: rm [-fiRr]... [FILE...]\n");
+					return 1;
+			}
+		}
+	}
+	argv += optind;
+
+	int status = 0;
+	for (char **file = argv; *file; file++) {
+		if (access(*file, F_OK) != 0 && !mode[force]) {
+			fprintf(stderr, "rm: cannot remove '%s' No such file or directory\n", *file);
+			status = 1;
+			continue;
+		}
+
+		if (mode[force] && (mode[recursive] || isfile(*file))) {
+			remove(*file);
+		} else if (mode[interactive] || access(*file, W_OK) != 0) {
+			fprintf(stderr, "rm: remove '%s'? ",* file);
+			if (getchar() != 'y')
+				continue;
+
+			if (isfile(*file) && !mode[recursive])
+				remove(*file);
+		}
+		// remove files if they are not dirs (unless recursive mode on)
+		if ((optind == 1 && isfile(*file)) || mode[recursive])
+			remove(*file);
+		else
+			status = 1;
+	}
+
+	return status;
 }
